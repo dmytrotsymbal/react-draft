993fdaa42299fdab9d8d6c22b8155b797a1a8546
Dima <dima.tsymbal@outlook.com>
2023-10-27 17:47:26 +0300

    loader and readme

---
+++ src/components/Loading.tsx
@@ -0,0 +1,7 @@
+import '../styles/Loading.scss'
+
+type Props = {}
+const Loading = (props: Props) => {
+    return <div className="custom-loader"></div>
+}
+export default Loading
--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,13 @@ import App from './App'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import store, { persistor } from 'redux/store'
+import Loading from 'components/Loading'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
+            <PersistGate loading={<Loading />} persistor={persistor}>
                 <App />
             </PersistGate>
         </Provider>
